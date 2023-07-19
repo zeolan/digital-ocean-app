047a1681aa7366c9cd5aaaff142941cb34162798
zeolan <zealan74@gmail.com>
2023-07-19 21:59:59 +0300

    Check script updated

--- src/data_check.js
+++ src/data_check.js
@@ -1,3 +1,34 @@
 const data = require("./data.json");
 
 console.log(`Number of Verbs ==> ${data.length}`);
+let roNameArrv = [];
+for (let i = 0; i < data.length; i++) {
+  if (roNameArrv.includes(data[i].nameRo[0])) {
+    console.error(`--------- Duplicated name ${data[i].nameRo[0]}`);
+  }
+  let missedPerson = "";
+  if (
+    ![
+      "eu",
+      "tu",
+      "el",
+      "noi",
+      "voi",
+      "ei",
+      "eu1",
+      "tu1",
+      "el1",
+      "noi1",
+      "voi1",
+      "ei1",
+    ].every((item) => {
+      missedPerson = item;
+      return Object.keys(data[i].conjugation).includes(item);
+    })
+  ) {
+    console.error(
+      `--------- Missed conjugation for person '${missedPerson}' for verb '${data[i].nameRo[0]}'`
+    );
+  }
+  roNameArrv.push(data[i].nameRo[0]);
+}
