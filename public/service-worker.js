7f653aa037e2ede597b40c13fb141b148170e84b
zeolan <zealan74@gmail.com>
2023-10-13 10:16:58 +0300

    Verbs added (309)

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 // Establish a cache name
 // !!! Need to increment CACHE_VERSION to force cache updates.
-const CACHE_VERSION = 13;
+const CACHE_VERSION = 14;
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
 const cacheFiles = [
