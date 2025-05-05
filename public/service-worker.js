61e9a1353a6cbfbae44600de7e02c7ae58769e2b
zeolan <zealan74@gmail.com>
2025-05-05 18:12:28 +0300

    Verbs added 423

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,9 @@
 // Establish a cache name
 // !!! Need to increment CACHE_VERSION to force cache updates.
-const CACHE_VERSION = 418;
+const CACHE_VERSION = 419;
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
-const cacheFiles = [
-  "./index.html",
-  "./manifest.json",
-  "./static/js/",
-];
+const cacheFiles = ["./index.html", "./manifest.json", "./static/js/"];
 
 self.addEventListener("install", (evt) => {
   return evt.waitUntil(
